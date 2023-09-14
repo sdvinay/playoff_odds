f9b7455abc4118b5e1e9fc26c053b9166cf55bdf
Vinay Kumar <sdvinay@gmail.com>
2023-09-14 13:11:02 -0700

    Move some utility/calculation code out of the simulator itself

--- season_simulator.py
+++ season_simulator.py
@@ -7,21 +7,7 @@ import numpy as np
 import series_probs_compute as probs
 import datasource_538 as ds
 import random
-
-
-def compute_standings(gms_played):
-    margins = gms_played['score1']-gms_played['score2']
-    winners = pd.Series(np.where(margins>0, gms_played['team1'], gms_played['team2']))
-    losers  = pd.Series(np.where(margins<0, gms_played['team1'], gms_played['team2']))
-    standings = pd.concat([winners.value_counts().rename('W'), losers.value_counts().rename('L')], axis=1).fillna(0)
-    standings.index.name = 'team'
-    standings['wpct'] = standings['W']/standings.sum(axis=1)
-    return standings.sort_values('wpct', ascending=False)
-
-
-def h2h_standings(games, teams):
-    return compute_standings(games.query('team1 in @teams and team2 in @teams'))
-
+import sim_utils
 
 #tie_breakers = {}
 #def break_tie(teams):
@@ -58,18 +44,6 @@ def summarize_results(standings):
     return summary.rename(columns={i: f'r{i}' for i in range(100)})
 
 
-def compute_standings_from_results(sim_results, incoming_standings):
-    standings = pd.concat([sim_results.groupby('iter')[col].value_counts().rename(col) for col in ('W', 'L')], axis=1)
-    standings.index.names = ['iter', 'team']
-    for col in standings.columns:
-        standings[col] = standings[col].fillna(0).astype(int)
-
-    iters = standings.reset_index()['iter'].unique()
-    stds_iterated = pd.concat([incoming_standings] * len(iters))
-    stds_iterated['iter'] = np.concatenate([np.repeat(i, len(incoming_standings)) for i in iters])
-    stds_iterated = stds_iterated.reset_index().set_index(['iter', 'team'])
-    full_standings = stds_iterated + standings
-    return full_standings
 
 def add_division_winners(sim_results):
     sim_results['div_win'] = False
@@ -153,7 +127,7 @@ def add_variation_to_ratings(ratings):
 def main(num_seasons: int = 100, save_output: bool = False, save_summary: bool = True, save_ranks: bool = True, id: int = 0, show_summary: bool = True, vary_ratings: bool = False):
     print(f'Simulating {num_seasons} seasons as ID {id}')
     (played, remain) = ds.get_games()
-    cur_standings = compute_standings(played)
+    cur_standings = sim_utils.compute_standings(played)
     ratings = ds.get_ratings()
     if vary_ratings:
         ratings = add_variation_to_ratings(ratings)
@@ -162,7 +136,7 @@ def main(num_seasons: int = 100, save_output: bool = False, save_summary: bool =
     sim_results = sim_n_seasons(remain, num_seasons)
     sim_results['job_id'] = id
 
-    standings = compute_standings_from_results(sim_results, cur_standings)
+    standings = sim_utils.compute_standings_from_results(sim_results, cur_standings)
     standings['job_id'] = id
     standings = process_sim_results(standings.reset_index())
 
---
+++ sim_utils.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+def compute_standings(gms_played):
+    margins = gms_played['score1']-gms_played['score2']
+    winners = pd.Series(np.where(margins>0, gms_played['team1'], gms_played['team2']))
+    losers  = pd.Series(np.where(margins<0, gms_played['team1'], gms_played['team2']))
+    standings = pd.concat([winners.value_counts().rename('W'), losers.value_counts().rename('L')], axis=1).fillna(0)
+    standings.index.name = 'team'
+    standings['wpct'] = standings['W']/standings.sum(axis=1)
+    return standings.sort_values('wpct', ascending=False)
+
+
+def h2h_standings(games, teams):
+    return compute_standings(games.query('team1 in @teams and team2 in @teams'))
+
+
+
+def compute_standings_from_results(sim_results, incoming_standings):
+    standings = pd.concat([sim_results.groupby('iter')[col].value_counts().rename(col) for col in ('W', 'L')], axis=1)
+    standings.index.names = ['iter', 'team']
+    for col in standings.columns:
+        standings[col] = standings[col].fillna(0).astype(int)
+
+    iters = standings.reset_index()['iter'].unique()
+    stds_iterated = pd.concat([incoming_standings] * len(iters))
+    stds_iterated['iter'] = np.concatenate([np.repeat(i, len(incoming_standings)) for i in iters])
+    stds_iterated = stds_iterated.reset_index().set_index(['iter', 'team'])
+    full_standings = stds_iterated + standings
+    return full_standings
