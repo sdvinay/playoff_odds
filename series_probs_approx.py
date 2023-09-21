b35fdaf55f682de75922c69ae79af2a0370d633f
Vinay Kumar <sdvinay@gmail.com>
2023-09-21 11:24:04 -0700

    Update coeffs for series probs approx

--- series_probs_approx.py
+++ series_probs_approx.py
@@ -2,16 +2,14 @@
 # Based on the analysis in approximate_series_probs.ipynb
 
 def p_series(games, r_home, r_away):
-    consts = {3: {'intercept': 0.5487166161582245,
-        'coeffs': [ 2.18372656e-03, -1.50440176e-06]},
-        5: {'intercept': 0.5118946934380574,
-        'coeffs': [ 2.77425761e-03, -2.80645606e-06]},
-        7: {'intercept': 0.5096396566543989,
-        'coeffs': [ 3.26422337e-03, -4.25017182e-06]}}
+    consts = {3: {'intercept': 0.55156, 'coeffs': [ 2.1364e-03, -7.7766e-07, -1.0786e-08]},
+        5: {'intercept': 0.51293, 'coeffs': [ 2.6860e-03, -2.8315e-07, -2.0100e-08]},
+        7: {'intercept': 0.51075, 'coeffs': [ 3.1303e-03, -3.0320e-07, -3.0410e-08]}}
 
     diff = r_home - r_away
-    diff_sq = abs(diff) * diff
+    diff_sq = diff * diff
+    diff_cu = diff * diff * diff
 
     cs = consts[games]
-    p = cs['intercept'] + diff * cs['coeffs'][0] + diff_sq * cs['coeffs'][1]
+    p = cs['intercept'] + diff * cs['coeffs'][0] + diff_sq * cs['coeffs'][1] + diff_cu * cs['coeffs'][2]
     return p
