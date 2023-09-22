cbef8acfc9875b71fd54d4a52ebd4562cf3ee2b3
Vinay Kumar <sdvinay@gmail.com>
2023-09-22 06:11:25 -0700

    hide internals of tiebreakers.py

--- tiebreakers.py
+++ tiebreakers.py
@@ -2,7 +2,7 @@ import datasource_mlb as ds
 import sim_utils
 import random
 
-def check_tie_breaker(teams):
+def __check_tie_breaker(teams):
     cur, remain = ds.get_games()
     h2h = sim_utils.h2h_standings(cur, teams)
     if len(h2h) > 0:
@@ -13,30 +13,29 @@ def check_tie_breaker(teams):
             return leader.name
         
 
-def find_all_clinched_tie_breakers():
+def __find_all_clinched_tie_breakers():
     clinched_tie_breakers = {}
     for lg in ds.league_structure['lg'].unique():
         tms = ds.league_structure.query("lg==@lg").index
         for tm1 in tms:
             for tm2 in tms:
                 if tm1 < tm2:
-                    tb = check_tie_breaker([tm1, tm2])
+                    tb = __check_tie_breaker([tm1, tm2])
                     if tb:
                         clinched_tie_breakers[(tm1, tm2)] = tb
     return clinched_tie_breakers
 
-clinched_tie_breakers = find_all_clinched_tie_breakers()
-clinched_tie_breakers[('SEA', 'TOR')] = 'SEA'
-clinched_tie_breakers[('BOS', 'SEA')] = 'SEA'
-clinched_tie_breakers[('BAL', 'TEX')] = 'BAL'
-clinched_tie_breakers[('CIN', 'MIA')] = 'MIA'
-clinched_tie_breakers[('MIA', 'SF')] = 'SF'
-
+__clinched_tie_breakers = __find_all_clinched_tie_breakers()
+__clinched_tie_breakers[('SEA', 'TOR')] = 'SEA'
+__clinched_tie_breakers[('BOS', 'SEA')] = 'SEA'
+__clinched_tie_breakers[('BAL', 'TEX')] = 'BAL'
+__clinched_tie_breakers[('CIN', 'MIA')] = 'MIA'
+__clinched_tie_breakers[('MIA', 'SF')] = 'SF'
 
 def break_tie(teams):
     tms = tuple(sorted(teams))
-    if tms in clinched_tie_breakers:
-        winner = clinched_tie_breakers[tms]
+    if tms in __clinched_tie_breakers:
+        winner = __clinched_tie_breakers[tms]
         return [winner] + [tm for tm in tms if tm != winner]
     return random.sample(tms, len(teams))
 
