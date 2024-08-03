ff05f14ec5ed7faf36cf08935216347f1a830fb9
Vinay Kumar <sdvinay@gmail.com>
2024-08-03 09:05:56 -0700

    Merge current standings into summary

--- summarize_results.py
+++ summarize_results.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import season_simulator as sim
-import playoff_simulator as psim
+import datasource as ds
+import sim_utils as su
 
 
 # Count the number of div/wc/playoff appearances by team from a set of results
@@ -29,10 +29,27 @@ def augment_summary(summary):
     summary['mean'] = summary['sum']/summary['len']
 
     # Get ratings for display purposes
-    ratings = sim.ds.get_ratings()
+    ratings = ds.get_ratings()
     summary = pd.merge(left=summary, right=ratings.astype(int), left_index=True, right_index=True)
-    cols = ['rating', 'mean', 'max', 'min'] + ['div_wins', 'byes', 'playoffs', 'lds_shares', 'lcs_shares', 'pennant_shares', 'ws_shares', 'p_home_game']
+
+    # Get standings
+    standings = get_standings_for_display()
+    summary = pd.merge(left=standings, right=summary, left_index=True, right_index=True)
+    cols = ['W', 'L', 'GB', 'rating', 'mean', 'max', 'min'] + ['div_wins', 'byes', 'playoffs', 'lds_shares', 'lcs_shares', 'pennant_shares', 'ws_shares', 'p_home_game']
     return summary[cols].sort_values(['ws_shares', 'mean'], ascending=False)
+
+
+def get_standings_for_display():
+    (cur, remain) = ds.get_games()
+    standings = pd.merge(left=ds.league_structure, right=su.compute_standings(cur),left_index=True, right_index=True)
+    standings = standings.sort_values(by=['div', 'wpct'], ascending=[True, False])
+    standings['over500'] = standings['W'] - standings['L']
+
+    # compute GB
+    div_leaders = standings.groupby('div')['over500'].transform(max)
+    standings['GB'] = (div_leaders - standings['over500'])/2
+    return standings
+    
 
 def restructure_results(sim_results):
     wins = sim_results['W'].unstack(level='team')
